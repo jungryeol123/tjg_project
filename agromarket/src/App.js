5caeb301db1adacbb3640b4a5b1c25956aabce49
yoon <46823971@naver.com>
2025-10-28 16:44:58 +0900

    modified commit

--- agromarket/src/App.js
+++ agromarket/src/App.js
@@ -1,13 +1,23 @@
-// import './styles/main.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Layout } from 'layouts/Layout';
 import Home from 'pages/Home';
 import { Delivery } from 'pages/Delivery';
 import { Login } from 'pages/Login';
+import { Signup } from 'pages/Signup';
 import KakaoCallback from 'features/auth/Kakao';
 import { useEffect } from 'react';
 import PaymentButton from 'features/order/PaymentButton';
 import { ProductDetail } from 'pages/ProductDetail';
+import { HeaderProductList } from 'pages/productCategoryList/HeaderProductList';
+import SearchResult from 'pages/searchResult/SearchResult';
+import CustomerService from 'pages/customerService/CustomerService';
+import {NoticeList} from 'pages/noticeList/NoticeList';
+import NoticeDetail from 'pages/noticeList/NoticeDetail';
+import { Cart } from 'pages/Cart';
+import { CheckoutInfo } from 'pages/CheckOutInfo';
+import { FindUserId } from 'pages/FindUserId';
+import { FindPassword } from 'pages/FindPassword';
+
 function App() {
 
    useEffect(() => {
@@ -27,11 +37,21 @@ function App() {
           <Route index element={<Home/>}/>
           <Route path="/delivery" element={<Delivery/>}/>
           <Route path="/login" element={<Login/>} />
+          <Route path="/find-user-id" element={<FindUserId />} />
+          <Route path="/find-password" element={<FindPassword />} />
+          <Route path="/signup" element={<Signup/>} />
           <Route path="/detail" element={<ProductDetail/>} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path='/checkout' element={<CheckoutInfo />} />
           <Route path="/features/auth/Kakao" element={<KakaoCallback />} />
           <Route path="/pay" element={<PaymentButton/>} />
           <Route path="/" element={<PaymentButton/>} />
-          <Route path="/products/:pid" element={<ProductDetail />} />
+          <Route path="/products/:pid/:id" element={<ProductDetail />} />
+          <Route path="/productList/:id" element={<HeaderProductList />} />
+           <Route path="/search/:keyword" element={<SearchResult />} />
+             <Route path="/support" element={<CustomerService />} />
+             <Route path="/notice" element={<NoticeList />} />
+                <Route path="/notice/:id" element={<NoticeDetail />} />
         </Route>
         </Routes>
       </BrowserRouter>
