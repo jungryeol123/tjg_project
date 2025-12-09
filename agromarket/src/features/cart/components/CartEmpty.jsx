64ed2aca2d38065dc9e473f326e5e60cec436b61
LEE <lsslcs01@gmail.com>
2025-12-09 10:33:45 +0900

    20251205 fix: CartEmpty.jsx scss적용

--- agromarket/src/features/cart/components/CartEmpty.jsx
+++ agromarket/src/features/cart/components/CartEmpty.jsx
@@ -1,9 +1,11 @@
 // features/cart/components/CartEmpty.jsx
 export function CartEmpty() {
   return (
-    <div className="cart-empty">
-      <p>장바구니에 담은 상품이 없습니다.</p>
-      <img src="/images/emptycart.png" style={{ marginTop: "20px" }} />
+    <div className="cart-right">
+      <div className="cart-empty">
+        <p>장바구니에 담은 상품이 없습니다.</p>
+        <img src="/images/emptycart.png" style={{ marginTop: "20px" }} />
+      </div>
     </div>
   );
 }
